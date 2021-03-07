039b3c3c6de435c528c9bdacdad61c1195bbe2d6
nguyenlekhtn <nguyenlekhtn@gmail.com>
2021-03-07 11:50:58 +0700

    Complete removeFromArrray test

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(...arguments) {
+    let array = arguments[0];
+    
+    for(let i = 1, n = arguments.length; i < n; i++)
+    {
+        // loop through array and compare each element with the argument
+        for(let j = 0, arrayLength = array.length; j < arrayLength; j++)
+        {
+            if(array[j] === arguments[i]) array.splice(j,1);
+        }
+    }
 
+    return array;
 }
 
 module.exports = removeFromArray
