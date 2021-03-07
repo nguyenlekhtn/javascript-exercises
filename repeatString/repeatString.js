1092a9f2d878b34315a69854fcf8f71a6bf004d7
nguyenlekhtn <nguyenlekhtn@gmail.com>
2021-03-07 10:19:42 +0700

    Complete helloWorld and repeatString challenges

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(text, times) {
 
+    let newText = "";
+    if(times < 0) return "ERROR";
+
+    for(let i = 0; i < times; i++) {
+        newText += text;
+    }
+    return newText;
 }
 
 module.exports = repeatString
