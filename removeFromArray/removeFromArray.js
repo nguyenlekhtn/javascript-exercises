d65a15fdcb797e9cbaaa55dc8678e7f3b2fd3247
nguyenlekhtn <nguyenlekhtn@gmail.com>
2021-03-07 11:53:19 +0700

    Change removeFromArray, use rest parameters

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,7 +1,7 @@
-const removeFromArray = function(...arguments) {
-    let array = arguments[0];
+const removeFromArray = function(array, ...arguments) {
+    // let array = arguments[0];
     
-    for(let i = 1, n = arguments.length; i < n; i++)
+    for(let i = 0, n = arguments.length; i < n; i++)
     {
         // loop through array and compare each element with the argument
         for(let j = 0, arrayLength = array.length; j < arrayLength; j++)
