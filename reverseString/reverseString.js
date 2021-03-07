080058eeaf4722d2ec44c2df0cee8b94ce262a63
nguyenlekhtn <nguyenlekhtn@gmail.com>
2021-03-07 11:07:46 +0700

    Complete reverseString test

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(text) {
+    let arrayText = text.split('');
+    let newArrayText = [];
+    while(arrayText.length != 0) {
+        newArrayText.push(arrayText.pop());
+    }
 
+    text = newArrayText.join('');
+    return text;
 }
 
 module.exports = reverseString
