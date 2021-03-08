26c6c896549c03cf18458bfb75130c58ede03bf1
nguyenlekhtn <nguyenlekhtn@gmail.com>
2021-03-08 10:17:07 +0700

    Complete leapYears test

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
+const leapYears = function(years) {
+    isLeap = false;
+    if (years % 4 == 0) {
+        isLeap = true;
+        if (years % 100 == 0) {
+            isLeap = false;
+            if (years % 400 == 0) {
+                isLeap = true;
+            }
+        }
+    }
 
+    return isLeap;
 }
 
 module.exports = leapYears
