4ab996dc21a35eaa6960902490f4d35afd85a91c
nguyenlekhtn <nguyenlekhtn@gmail.com>
2021-03-08 09:57:42 +0700

    Complete sumAll test

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(intA, intB) {
 
+    if(intA < 0 || intB < 0 || typeof intA !== "number" || typeof intB !== "number" ) return "ERROR";
+
+    let bigger = (intA > intB) ? intA : intB;
+    let smaller = (intA > intB) ? intB : intA;
+   
+    let sum = 0;
+    for(let i = smaller; i <= bigger; i++) {
+        sum += i;
+    }
+
+    return sum;
 }
 
 module.exports = sumAll
